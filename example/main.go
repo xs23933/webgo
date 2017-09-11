a2d952b032e6d5ea3f56b66168d7bd63a2d0d066
xs23933 <xs23933@gmail.com>
2017-09-11 23:02:31 +0800

    add something

--- example/main.go
+++ example/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"webgo"
+	"github.com/xs23933/webgo"
 )
 
 type MainHandler struct {
@@ -10,10 +9,20 @@ type MainHandler struct {
 }
 
 func (p *MainHandler) Get() {
-	fmt.Println("MainHandler.Get")
-	p.Write("Hello world!")
+	p.Write(`<html>
+<head>
+<link href="/css/app.css" rel="stylesheet" />
+<script src='/js/app.js'></script>
+</head>
+<body>
+<h1>Hello world</h1>
+<img src='/images/logo.jpg' />
+</body>
+</html>
+	`)
 }
 func main() {
 	webgo.Route("/", &MainHandler{})
+	webgo.Route("/user/", &MainHandler{})
 	webgo.Run()
 }
